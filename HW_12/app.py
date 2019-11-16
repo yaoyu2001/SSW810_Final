5683941aaab2c98bd22b4c23bde1fb1a2cf58f34
yaoyu2001 <yongchangyao2001@gmail.com>
2019-11-16 15:45:12 -0500

    HW_12 V 1.0

---
+++ HW_12/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/instructor_courses')
+def template_demo():
+    db_path = "G:\Interview\Data\810_startup_Yongchang_Yao.db"
+    try:
+        db = sqlite3.connect(db_path)
+    except sqlite3.OperationalError:
+        print(f"Error: Unable to open database at {db_path}")
+    else:
+       query ="""select i.CWID,i.Name,i.Dept,g.Course,count(*) as Students from grades g join instructors i on g.InstructorCWID=i.CWID
+                                        group by i.CWID,i.Name,i.Dept,g.Course"""
+    data = [{'cwid': cwid, 'name': name, 'dept': dept, "courses": courses, "students": students}
+                for cwid, name, dept, courses, students in db.execute(query)]
+    db.close()
+    return render_template('instructors_summary.html', title='Stevens Repository',
+                           table_title="Number of student by courses and instructor",students = data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
